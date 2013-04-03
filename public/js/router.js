9876d79c23f131da5a4764dbf6ae36e4d223d893
hrbarz <hr.barz@gmail.com>
2013-04-03 08:56:43 -0300

    App Js
    Ttemporary reorganization js app

--- public/js/router.js
+++ public/js/router.js
@@ -3,77 +3,27 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'views/tasklistView'
-
-], function($, _, Backbone,TasklistView) {
-
-
-  
+	'vm'
+], function ($, _, Backbone, Vm) {
   var AppRouter = Backbone.Router.extend({
     routes: {
-      // Define some URL routes
-      
-      'home': 'showHome',
-      
-      // Default
-      '*actions': 'defaultAction'
+      '*actions': 'defaultAction' // All urls will trigger this route
     }
   });
-  
-  var initialize = function(){
 
-    var app_router = new AppRouter;
+  var initialize = function(options){
+		var appView = options.appView;
+    var router = new AppRouter(options);
+
+		router.on('route:defaultAction', function (actions) {
+			require(['views/dashboard/page'], function (DashboardPage) {
+        var dashboardPage = Vm.create(appView, 'DashboardPage', DashboardPage);
+        dashboardPage.render();
+      });
+		});
     
-    app_router.on('route:showHome', function(){
-   
-
-    });
-
-    app_router.on('route:defaultAction', function (actions) {
-
-      //TasklistView.render();
-
-      var projectsView = new TasklistView();
-        projectsView.render();
-
-
-      //alert('a todo'+ decodeURIComponent(actions));
-      //var taskListView = new TaskListView();
-      //taskListView.render();
-
-    });
-
-    Backbone.history.start({pushState: true, root: "/"});
-
-
-
-    /*if (Backbone.history && Backbone.history._hasPushState) {
-   
-      // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
-      $(document).delegate("a", "click", function(evt) {
-
-        // Get the anchor href and protcol
-        var href = $(this).attr("href");
-        var protocol = this.protocol + "//";
-     
-        // Ensure the protocol is not part of URL, meaning its relative.
-        // Stop the event bubbling to ensure the link will not cause a page refresh.
-        if (href.slice(protocol.length) !== protocol) {
-          evt.preventDefault();
-     
-          // Note by using Backbone.history.navigate, router events will not be
-          // triggered.  If this is a problem, change this to navigate on your
-          // router.
-            Backbone.history.navigate(href, true);
-          }
-        });
-     
-    }*/
-
-
-
   };
-  return { 
+  return {
     initialize: initialize
   };
 });
